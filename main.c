0fde333e306f4c21dfa376579adc793c62b1d792
MireilleVignal <mireille-vignal@orange.fr>
2019-10-09 10:59:41 +0200

    Création de la fenêtre d'affichage

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
-#import <stdlib.h>
-#import <stdio.h>
-#import <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 #include "SDL.h"
 
 int main()
@@ -9,6 +9,9 @@ int main()
 		printf("Failed to initialize SDL: %s\n", SDL_GetError());
 		return -1;
 	}
+	SDL_Window *screen = SDL_CreateWindow("Mini_RPG",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 600, SDL_WINDOW_SHOWN);
+
+	SDL_Delay(4000);
 
 	SDL_Quit();
 }
