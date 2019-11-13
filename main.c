2e75795cc3d0ecd864f69e9d4de638bb53c1cad3
Amine Mohamed Larbi <larbi9u@fst-ats-303-l18.fst.site.univ-lorraine.fr>
2019-11-13 11:33:33 +0100

    affichage d'une sprite de notre personnage

--- main.c
+++ main.c
@@ -4,95 +4,64 @@
 #include <SDL2/SDL.h>
 #include <SDL.h>
 
+const int SHEET_WIDTH = 1536;
+const int SHEET_HEIGHT = 2112;
 
 int main(int argc, char** argv)
-{
+{ 
     bool quit = false;
+    SDL_Event event;
     
-   /* Initialisation simple */
+    
     if (SDL_Init(SDL_INIT_VIDEO) != 0 )
     {
         fprintf(stdout,"Échec de l'initialisation de la SDL (%s)\n",SDL_GetError());
         return -1;
     }
-
-    {
-        /* Création de la fenêtre */
-        SDL_Window* window = NULL;
-        window = SDL_CreateWindow("minirpg",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,640,480,SDL_WINDOW_SHOWN);
-        
-        SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
-        SDL_Surface * image = SDL_LoadBMP("tux-sit.bmp");
-        SDL_Texture* monImage = SDL_CreateTextureFromSurface(renderer,image);
-        
-        while (!quit){
-    
-            SDL_RenderCopy(renderer, monImage, NULL, NULL);
-            SDL_RenderPresent(renderer);
-        }
  
-        SDL_DestroyTexture(monImage);
-        SDL_FreeSurface(image);
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        
-        
-        if( window )
+    SDL_Init(SDL_INIT_VIDEO);
+    /* Création de la fenêtre */
+    SDL_Window * window = SDL_CreateWindow("miniRPG",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_Surface * image = SDL_LoadBMP("nEFACHdg.bmp");
+    SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, image);
+    
+    //destination region
+    SDL_Rect dstrect = { 10, 10, 64, 48 };
+    SDL_Rect srcrect = { 0, 0, SHEET_WIDTH/24, SHEET_HEIGHT/30.17 };
+    while (!quit)
+    {
+        SDL_WaitEvent(&event);
+ 
+        switch (event.type)
         {
-            SDL_Delay(3000); /* Attendre trois secondes, que l'utilisateur voie la fenêtre */
-
+        case SDL_QUIT:
+            quit = true;
+            break;
+        }
+        
+        SDL_RenderCopy(renderer, texture, &srcrect, &dstrect);
+        SDL_RenderPresent(renderer);
+    }
+    
+    /* Attendre trois secondes, que l'utilisateur voie la fenêtre + Cas de l'erreur de la création de la fenêtre */
+    if( window )
+        {
+            SDL_Delay(3000);  
             SDL_DestroyWindow(window);
         }
         else
         {
             fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError());
         }
-    }
     
- /* 
-//PERSONNAGE -----------------------------------------------------------------------------------------------------------------------
-    
-    //attribus de l'image des Sprites
-    const int SHEET_WIDTH = 1536;
-    const int SHEET_HEIGHT = 2112;
-    
-    //Les surfaces
-    SDL_Surface *perso = NULL;
-    SDL_Surface *screen2 = NULL;
-    
-    //La structure d'événements
-    SDL_Event event;
-    
-    //partie qui sera blitté
-    SDL_Rect clip[1]; 
-
-
-    void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination, SDL_Rect* clip )
-    {
-
-    //On coupe un sprite dans notre 
-        clip[0].x = 0;
-        clip[0].y = SHEET_HEIGHT/30.17;
-        clip[0].w = SHEET_WIDTH/24;
-        clip[0].h = SHEET_HEIGHT/30.17;
-
-
-        SDL_Rect offset;
-    
-        offset.x = x;
-        offset.y = y;
-    
+    SDL_DestroyTexture(texture);
+    SDL_FreeSurface(image);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
  
- //On blitte la surface
-        SDL_BlitSurface( source, clip, destination, &offset );
-    }
-
-    //On applique les sprites sur l'écran
-    apply_surface(0,0, perso, screen2, &clip[0]);
-*/      
     SDL_Quit();
-
+ 
     return 0;
-    
-   
+
 }
